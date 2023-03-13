9f01eda6a4080af9b2c574c0b7686a7bc5b70d20
minthantkyaw28 <wanderhobby@gmail.com>
2023-03-13 14:48:25 +0630

    Commit 2

--- L1/L1.ino
+++ L1/L1.ino
@@ -1,31 +1,50 @@
 
 int led13=13;
+int led8=8;
+int led4=4;
+int led7=7;
+
 int count=0;
 
 void setup() {
   // put your setup code here, to run once:
-  //pinMode(led13,OUTPUT);
+  pinMode(led13,OUTPUT);
+  pinMode(led8,OUTPUT);
+  pinMode(led4,OUTPUT);
+  pinMode(led7,OUTPUT);
 
-  // Serial.begin(9600);
+  Serial.begin(9600);
 }
 
 void loop() {
-  //blinker(led13,700);
 
-  count=count+1;
+  blinker(led7,1000,500);
+  blinker(led13,1000,500);
+  blinker(led8,1000,500);
+  blinker(led4,1000,500);
 
-  Serial.print("Hello World !");
-  Serial.print(count);
-  Serial.print("\n");
+  // count=count+1;
+
+  // Serial.print("Hello World !");
+  // Serial.print(count);
+  // Serial.print("\n");
   
-  delay(1000);
+  // delay(1000);
 }
 
 
 // Custom Funcs
-void blinker(int pinNUM,int delayTime){
+void blinker(int pinNUM,int ONdelayTime,int OFFdelayTime){
   digitalWrite(pinNUM,HIGH);
-  delay(delayTime);
+  Serial.print("LED number ");
+  Serial.print(pinNUM);
+  Serial.print(" IS ON \n");
+  delay(ONdelayTime);
+
   digitalWrite(pinNUM,LOW);
-  delay(delayTime);
+  Serial.print("LED number ");
+  Serial.print(pinNUM);
+  Serial.print(" IS OFF \n");
+  delay(OFFdelayTime);
+
 }
