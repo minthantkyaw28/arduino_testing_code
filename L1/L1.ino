19f3e0f4e0b3fa331fc1da29b1dbb168de50124f
minthantkyaw28 <wanderhobby@gmail.com>
2023-03-14 13:52:44 +0630

    commit 3

--- L1/L1.ino
+++ L1/L1.ino
@@ -1,26 +1,30 @@
 
 int led13=13;
+int led12=12;
 int led8=8;
-int led4=4;
 int led7=7;
+int led4=4;
 
 int count=0;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(led13,OUTPUT);
+  pinMode(led12,OUTPUT);
   pinMode(led8,OUTPUT);
-  pinMode(led4,OUTPUT);
   pinMode(led7,OUTPUT);
+  pinMode(led4,OUTPUT);
 
   Serial.begin(9600);
 }
 
 void loop() {
 
-  blinker(led7,1000,500);
+
   blinker(led13,1000,500);
+  blinker(led12,1000,500);
   blinker(led8,1000,500);
+  blinker(led7,1000,500);
   blinker(led4,1000,500);
 
   // count=count+1;
