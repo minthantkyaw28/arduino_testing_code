5d7322ca010e7f89e3e8e81515a94aea4572d11e
minthantkyaw28 <wanderhobby@gmail.com>
2023-03-20 14:01:38 +0630

    Commit 7

---
+++ L1/L1_Ldr_Buzzer_Led.ino/L1_Ldr_Buzzer_Led.ino.ino
@@ -0,0 +1,25 @@
+int LDR = A0; // LDR is connected to analog pin A0
+int buzzer = 8; // Buzzer is connected to digital pin 8
+int led = 13; // LED is connected to digital pin 13
+
+void setup() {
+  pinMode(led, INPUT);
+  pinMode(buzzer, OUTPUT);
+  pinMode(led, OUTPUT);
+  Serial.begin(9600); // Initialize serial communication
+}
+
+void loop() {
+  int LDR_value = analogRead(LDR); // Read LDR value
+  Serial.println(LDR_value); // Print LDR value to serial monitor
+  delay(50); // Wait for a moment
+
+  if (LDR_value > 800) { // If LDR value is greater than 800
+    digitalWrite(buzzer, HIGH); // Turn on the buzzer
+    digitalWrite(led, HIGH); // Turn on the LED
+    delay(100); // Wait for 1 second
+    digitalWrite(buzzer, LOW); // Turn off the buzzer
+    digitalWrite(led, LOW);
+    delay(100);// Turn off the LED
+  }
+}
