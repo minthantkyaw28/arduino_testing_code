ca84411fc142910379b651c1fed433cc3a767a4d
minthantkyaw28 <wanderhobby@gmail.com>
2023-03-25 14:54:57 +0630

    Arduino day 2023 _2

--- L1/arduino_day_2023_sketches/arduino_day_2023_sketches.ino
+++ L1/arduino_day_2023_sketches/arduino_day_2023_sketches.ino
@@ -1,22 +1,39 @@
 // C++ code
 //
 
-const int pin9=9;
-const int pin8=8;
+// 2 Leds 
+// const int pin9=9;
+// const int pin8=8;
 
+// void setup()
+// {
+//   pinMode(pin9, OUTPUT);
+//    pinMode(pin8, OUTPUT);
+// }
+
+// void loop()
+// {
+//   digitalWrite(pin9, HIGH);
+//   digitalWrite(pin8, LOW);
+//   delay(500); // Wait for 1000 millisecond(s)
+//   digitalWrite(pin9, LOW);
+//   digitalWrite(pin8, HIGH);
+//   delay(500); 
+//   // Wait for 1000 millisecond(s)
+// }
+
+//Blink built in 13 
+// 13 == LED_BUILTIN
 void setup()
 {
-  pinMode(pin9, OUTPUT);
-   pinMode(pin8, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+
 }
 
 void loop()
 {
-  digitalWrite(pin9, HIGH);
-  digitalWrite(pin8, LOW);
-  delay(500); // Wait for 1000 millisecond(s)
-  digitalWrite(pin9, LOW);
-  digitalWrite(pin8, HIGH);
-  delay(500); 
-  // Wait for 1000 millisecond(s)
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);  // Wait for 1000 millisecond(s)
 }
