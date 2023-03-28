31ec6c66f5415556562f1467ef0c2eebcf2c8da8
minthantkyaw28 <wanderhobby@gmail.com>
2023-03-28 14:36:14 +0630

    PIR testing day

--- L1/PIR_test.ino/PIR_test.ino.ino
+++ L1/PIR_test.ino/PIR_test.ino.ino
@@ -1,9 +1,11 @@
 int pirPin = 2; // Define PIR sensor pin
 int ledPin = 13; // Define LED pin
+int buzzarPin=4;
 
 void setup() {
   pinMode(pirPin, INPUT); // Set PIR sensor pin as input
   pinMode(ledPin, OUTPUT); // Set LED pin as output
+  pinMode(buzzarPin, OUTPUT); 
   Serial.begin(9600); // Start serial communication
 }
 
@@ -12,10 +14,13 @@ void loop() {
   
   if (pirState == HIGH) { // If motion is detected
     digitalWrite(ledPin, HIGH); // Turn on LED
+    digitalWrite(buzzarPin, HIGH); 
     Serial.println("Motion detected!"); // Print message to serial monitor
   } else {
     digitalWrite(ledPin, LOW); // Turn off LED
+    digitalWrite(buzzarPin, LOW); 
+    Serial.println("Motionless !");
   }
   
-  delay(100); // Wait for 100ms before reading PIR sensor again
+  delay(10); // Wait for 100ms before reading PIR sensor again
 }
